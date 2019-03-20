538408aac1331a1954d24887389b158d4adafeda
Gregory Mertens <info@mertens3d.com>
2019-03-20 09:50:00 -0500

    end of module 6

--- OdeToFood/Models/RestaurantReview.cs
+++ OdeToFood/Models/RestaurantReview.cs
@@ -7,30 +7,30 @@ using System.Web;
 namespace OdeToFood.Models
 {
 
-    public class MaxWordsAttribute : ValidationAttribute
-    {
-        public MaxWordsAttribute(int maxWords):base("{0} has too many words.")
-        {
-            _maxWords = maxWords;
-        }
+    //public class MaxWordsAttribute : ValidationAttribute
+    //{
+    //    public MaxWordsAttribute(int maxWords):base("{0} has too many words.")
+    //    {
+    //        _maxWords = maxWords;
+    //    }
 
-        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
-        {
-            if (value != null)
-            {
-                var valueAsString = value.ToString();
-                if (valueAsString.Split(' ').Length > _maxWords)
-                {
-                    var errorMessage = FormatErrorMessage(validationContext.DisplayName);
-                    return new ValidationResult(errorMessage);
-                }
-            }
-            return ValidationResult.Success;
-        }
+    //    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    //    {
+    //        if (value != null)
+    //        {
+    //            var valueAsString = value.ToString();
+    //            if (valueAsString.Split(' ').Length > _maxWords)
+    //            {
+    //                var errorMessage = FormatErrorMessage(validationContext.DisplayName);
+    //                return new ValidationResult(errorMessage);
+    //            }
+    //        }
+    //        return ValidationResult.Success;
+    //    }
 
-        private readonly int _maxWords;
-    }
-    public class RestaurantReview
+    //    private readonly int _maxWords;
+    //}
+    public class RestaurantReview :IValidatableObject
     {
 
         public int Id { get; set; }
@@ -43,8 +43,11 @@ namespace OdeToFood.Models
         public int RestaurantId { get; set; }
         [Display(Name="User Name")]
         [DisplayFormat(NullDisplayText = "anonymous")]
-        [MaxWords(1)]
+        //[MaxWords(1)]
         public string ReviewerName { get; set; }
 
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+        }
     }
 }
