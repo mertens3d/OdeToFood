899bbf35a7c523428b43f6e655269240ff33ff85
Gregory Mertens <info@mertens3d.com>
2019-03-20 14:18:49 -0500

    end of section 7

--- OdeToFood/App_Start/BundleConfig.cs
+++ OdeToFood/App_Start/BundleConfig.cs
@@ -8,7 +8,7 @@ namespace OdeToFood
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-                                         
+
             bundles.Add(new ScriptBundle("~/bundles/otf").Include(
                 "~/Scripts/jquery-{version}.js",
                 "~/Scripts/jquery-ui-{version}.js",
@@ -33,7 +33,9 @@ namespace OdeToFood
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css")
+                .Include("~/Content/site.css")
+                .Include("~/Content/PagedList.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PagedList;
 
 namespace OdeToFood.Controllers
 {
@@ -27,21 +28,21 @@ namespace OdeToFood.Controllers
             return Json(model, JsonRequestBehavior.AllowGet);
         }
 
-        public ActionResult Index(string searchTerm = null)
+        public ActionResult Index(string searchTerm = null, int page= 1)
         {
 
             var model = _db.Restaurants
                 .OrderByDescending(r => r.Reviews.Average(review => review.Rating))
-                .Where( r=> searchTerm == null || r.Name.StartsWith(searchTerm))
-                .Take(10)
+                .Where(r => searchTerm == null || r.Name.StartsWith(searchTerm))
                 .Select(r => new RestaurantListViewModel()
-                        {
-                            Id = r.Id,
-                            Name = r.Name,
-                            City = r.City,
-                            Country = r.Country,
-                            CountOfReviews = r.Reviews.Count()
-                        });
+                {
+                    Id = r.Id,
+                    Name = r.Name,
+                    City = r.City,
+                    Country = r.Country,
+                    CountOfReviews = r.Reviews.Count()
+                })
+                .ToPagedList(page, 10);
 
 
             if (Request.IsAjaxRequest())
